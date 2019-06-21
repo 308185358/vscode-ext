f1a4ff6d68a1d7dde2d4dd51b1b2f0795e414032
Hang <>
2019-06-21 19:21:00 +0800

    add go ext

---
+++ go-ext/vscode_go_env.sh
@@ -0,0 +1,16 @@
+go get -v -u github.com/golang/tools/go/buildutil
+
+mkdir $GOPATH/src/golang.org
+ln -s $GOPATH/src/github.com/golang $GOPATH/src/golang.org/x
+
+cd $GOPATH/src/golang.org/x/tools/go/buildutil
+go install
+
+
+
+# 在$GOPATH/src/github.com/golang/目录下clone
+cd $GOPATH/src/github.com/golang/
+git clone https://github.com/golang/lint.git
+
+# go install github.com/golang/lint/golint   
+go install golang.org/x/lint/golint
